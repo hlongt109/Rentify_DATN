95fc0e3e6771d3e28aadaeba3ee7cd20d040f13a
tampdph33277 <tampdph33277@fpt.edu.vn>
2024-10-25 23:17:44 +0700

    edit_name_file

--- Rentify Server/routes/api.js
+++ Rentify Server/routes/api.js
@@ -9,13 +9,13 @@ var router = express.Router();
 const userApi = require('./Api_Admin/demoApi');
 //NhanVien
 
-const post_staffApi = require('./Api_Staff/Post_Staff_Api');
-const contract_staffApi = require('./Api_Staff/Contract_Staff_Api');
-const payment_staffApi = require('./Api_Staff/Payment_Staff_Api');
-const notification_staffApi = require('./Api_Staff/Notification_Staff_Api');
-const room_staffApi = require('./Api_Staff/Room_Staff_Api');
-const invoice_staffApi = require('./Api_Staff/Invoice_Staff_Api');
-const request_staffApi = require('./Api_Staff/Request_Staff_Api');
+const post_staffApi = require('./Api_Staff/Post');
+const contract_staffApi = require('./Api_Staff/Contract');
+const payment_staffApi = require('./Api_Staff/Payment');
+const notification_staffApi = require('./Api_Staff/Notification');
+const room_staffApi = require('./Api_Staff/Room');
+const invoice_staffApi = require('./Api_Staff/Invoice');
+const request_staffApi = require('./Api_Staff/Request');
 // nối 
 router.use('/', userApi);
 router.use('/staff/posts', post_staffApi); // Đổi đường dẫn cho post_staffApi
