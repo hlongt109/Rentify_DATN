f37d87e9cc986522be01c8b0bcffe0a76ebfee3c
Vu Van Phuc <phucvuvan63@gmail.com>
2024-10-25 15:15:01 +0700

    hoàn thiện chức năng quản lý thanh toán của chủ tòa #25/10/2024

--- Rentify Server/routes/api.js
+++ Rentify Server/routes/api.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 const UserManage=require("./Api_BuildingOwner/UserManageApi")
 const PostManage= require("./Api_BuildingOwner/PostManageApi")
-// const PaymentManage= require("./Api_BuildingOwner/PaymentManageApi")
+const PaymentManage= require("./Api_BuildingOwner/PaymentManageApi")
 // const ContractManage= require("./Api_BuildingOwner/ContractManageApi")
 // const BuildingManage= require("./Api_BuildingOwner/BuildingManageApi")
 // nối 
 router.use('/',UserManage)
 router.use('/',PostManage)
-// router.use('/',PaymentManage)
+router.use('/',PaymentManage)
 // router.use('/',ContractManage)
 // router.use('/',BuildingManage)
 
