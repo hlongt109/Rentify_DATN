892bc6455fa3b2a1fbc1552279e2aa223d233fe5
Tran Hoang Long <hoanglong@smiletech.com>
2024-10-26 21:44:08 +0700

    commit Invoice

--- Rentify Server/models/Invoice.js
+++ Rentify Server/models/Invoice.js
@@ -7,7 +7,7 @@ const Invoice = new Schema({
     amount: { type: Number, require: true }, // số tien
     transaction_type: { type: String, enum: ['income', 'expense'], require: true },
     due_date: { type: String}, // hạn chót
-    payment_status: { type: String, enum: ['paid', 'unpaid', 'pending'] },
+    payment_status: { type: String, enum: ['paid', 'unpaid', 'pending'] }, // đã thanh toán, chưa thanh toán, đang chờ 
     created_at: { type: String }
 })
 module.exports = mongoose.model("Invoice", Invoice);
