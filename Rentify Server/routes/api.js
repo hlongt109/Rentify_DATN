6028b7d1c4122e1ce5f7684b887c42ae81b777f6
Nguyen Thien Thien <kinaad24@gmail.com>
2024-10-29 21:27:04 +0700

    merge complete

--- Rentify Server/routes/api.js
+++ Rentify Server/routes/api.js
@@ -10,7 +10,7 @@ const adminApi = require('./Api_Admin/loginApi');
 const postAdmin = require('./Api_Admin/postApi');
 const userAdmin = require('./Api_Admin/userApi');
 const nofAdmin = require('./Api_Admin/nofApi');
-const spAdmin = require('./Api_Admin/spApi');
+const spAdmin = require('./Api_Admin/supportApi');
 const statAdmin = require('./Api_Admin/statsApi');
 // building_owner
 const supportManageApi = require("./Api_BuildingOwner/support_manage_api")
@@ -40,12 +40,6 @@ const room_staffApi = require('./Api_Staff/Room');
 const invoice_staffApi = require('./Api_Staff/Invoice');
 const request_staffApi = require('./Api_Staff/Request');
 const login_staffApi = require('./Api_Staff/login');
-const UserManage=require("./Api_BuildingOwner/UserManageApi")
-const PostManage= require("./Api_BuildingOwner/PostManageApi")
-const PaymentManage= require("./Api_BuildingOwner/PaymentManageApi")
-const ContractManage= require("./Api_BuildingOwner/ContractManageApi")
-const BuildingManage= require("./Api_BuildingOwner/BuildingManageApi")
-const notificationApi = require('./Api_BuildingOwner/notification_api');
 
 // nối 
 router.use('/', serviceApi)
@@ -58,6 +52,7 @@ router.use('', staffManageApi);
 router.use('', serviceManageApi);
 router.use('', statisticApi);
 router.use('', paymentApi_BuildingOwner);
+router.use('', UserManage);
 router.use('', notificationApi);
 router.use("/", apiUser);
 router.use('', adminApi);
