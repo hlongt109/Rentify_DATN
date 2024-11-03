46cf13cf65de8bd2747a9c5310eea9fff982996f
Nguyen Thien Thien <kinaad24@gmail.com>
2024-11-03 18:04:03 +0700

    thien push

--- Rentify Server/routes/Api_Admin/userApi.js
+++ Rentify Server/routes/Api_Admin/userApi.js
@@ -4,20 +4,14 @@ var router = express.Router();
 const User = require("../../models/User");
 //Lấy danh sách người dùng
 
-router.get("/user/show-list", async (req, res) => {
-    try {
-        const find = await User.find();
-        res.render('UserManagement/UserManagement', { find });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Lỗi server');
-    }
-});
-
 router.get("/user/list", async (req, res) => {
     try {
         const find = await User.find();
-        res.render('Accounts/listAccount', { showList: find });
+        return res.status(200).json({
+            status: 200,
+            message: "Lấy dữ liệu thành công",
+            find
+        });
     } catch (error) {
         console.error(error);
         res.status(500).send('Lỗi server');
@@ -25,17 +19,6 @@ router.get("/user/list", async (req, res) => {
 });
 
 //sửa role người dùng
-router.get("/user/update1/:id", async (req, res) => {
-    const postId = req.params.id;
-    const upDB = await User.findById(postId); // Tìm bài viết theo ID
-
-    if (!upDB) {
-        return res.status(404).json({ message: 'Account không tồn tại' });
-    }
-
-    res.render("Accounts/updateAccount", { upDB });
-})
-// Sửa role người dùng
 router.put("/user/update/:id", async (req, res) => {
     try {
         const findID = req.params.id;
@@ -44,17 +27,13 @@ router.put("/user/update/:id", async (req, res) => {
         // Tìm kiếm người dùng theo ID và cập nhật role
         const upDB = await User.findByIdAndUpdate(
             findID,
-            { role },// thứ cần sửa ở đây
-            { new: true, runValidators: true }
+            { role }, // Chỉ cập nhật trường role
+            { new: true, runValidators: true } // new: true để trả về đối tượng cập nhật, runValidators: true để chạy các validators
         );
 
         if (!upDB) {
             return res.status(404).json({ message: 'Không tìm thấy tài khoản' });
         }
-        // let msg = 'Sửa thành công id: ' + findID;
-        // console.log(msg);
-        // return res.status(200).json({ message: 'Cập nhật thành công' });
-        res.redirect("/api/user/list");
 
         let msg = 'Sửa thành công id: ' + findID;
         console.log(msg);
@@ -64,6 +43,8 @@ router.put("/user/update/:id", async (req, res) => {
         return res.status(500).json({ message: msg });
     }
 });
+
+
 router.delete("/user/delete/:id", async (req, res) => {
     try {
         const findID = req.params.id;
--- Rentify Server/routes/index.js
+++ Rentify Server/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const Post = require('../models/Post')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // Gọi route /api/home để lấy nội dung cho body
@@ -31,28 +33,22 @@ router.get('/api/user/show-list', (req, res) => {
   });
 });
 
-router.get('/api/user/show-list', (req, res) => {
-  res.render('UserManagement/UserManagement', (err, html) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.render('index', {
-      title: 'Quản lý người dùng',
-      body: html 
-    });
-  });
-});
-
-router.get('/post/list', (req, res) => {
-  res.render('Posts/listPost', { list }, (err, html) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.render('index', {
-      title: 'Quản lý người dùng',
-      body: html
-    });
-  });
+router.get('/api/post/list', async (req, res) => {
+  try {
+      const showList = await Post.find(); // Lấy danh sách bài đăng từ MongoDB
+      res.render('Posts/listPost', { list: showList }, (err, html) => { // Truyền biến list vào template
+          if (err) {
+              return res.status(500).send(err);
+          }
+          res.render('index', {
+              title: 'Quản lý bài đăng',
+              body: html 
+          });
+      });
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Lỗi server');
+  }
 });
 
 
