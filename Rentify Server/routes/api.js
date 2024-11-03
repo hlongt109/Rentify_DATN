f716626874cb8baa239515e1824e95ba4cc252e1
Tran Hoang Long <hoanglong@smiletech.com>
2024-11-04 01:22:10 +0700

    commit call api signUp

--- Rentify Server/routes/api.js
+++ Rentify Server/routes/api.js
@@ -6,9 +6,11 @@ var router = express.Router();
 // router.use('',tentenRouter)
 
 //============================================
-const adminApi = require('./Api_Admin/loginApi');
-const postAdmin = require('./Api_Admin/postApi');
-const userAdmin = require('./Api_Admin/userApi');
+const authApi = require("./Api_Auth/auth_Api");
+const adminApi = require("./Api_Admin/loginApi");
+const postAdmin = require("./Api_Admin/postApi");
+const userAdmin = require("./Api_Admin/userApi");
+const spAdmin = require("./Api_Admin/supportApi");
 // building_owner
 const supportManageApi = require("./Api_BuildingOwner/support_manage_api")
 const staffManageApi = require("./Api_BuildingOwner/staff_manage_api")
@@ -37,33 +39,36 @@ const room_staffApi = require('./Api_Staff/Room');
 const invoice_staffApi = require('./Api_Staff/Invoice');
 const request_staffApi = require('./Api_Staff/Request');
 
-// nối 
-router.use('/', serviceApi)
-router.use('/', roomApi)
-router.use('/', invoiceApi)
-router.use('/', reportApi)
-router.use('/', paymentApi)
-router.use('', supportManageApi);
-router.use('', staffManageApi);
-router.use('', serviceManageApi);
-router.use('', statisticApi);
-router.use('', paymentApi_BuildingOwner);
-router.use('', notificationApi);
-router.use("/", apiUser);
-router.use('', adminApi);
-router.use('', postAdmin);
-router.use('', userAdmin);
-router.use('/staff/posts', post_staffApi); // Đổi đường dẫn cho post_staffApi
-router.use('/staff/contracts', contract_staffApi); // Đổi đường dẫn cho contract_staffApi
-router.use('/staff/payments', payment_staffApi); // Đổi đường dẫn cho contract_staffApi
-router.use('/staff/notifications', notification_staffApi); // Đổi đường dẫn cho contract_staffApi
-router.use('/staff/rooms', room_staffApi); // Đổi đường dẫn cho contract_staffApi
-router.use('/staff/invoices', invoice_staffApi); // Đổi đường dẫn cho contract_staffApi
-router.use('/staff/requests', request_staffApi); // Đổi đường dẫn cho contract_staffApi
-router.use('/',UserManage)
-router.use('/',PostManage)
-router.use('/',PaymentManage)
-router.use('/',ContractManage)
-router.use('/',BuildingManage)
+// nối
+router.use("/", authApi);
+router.use("/", serviceApi);
+router.use("/", roomApi);
+router.use("/", invoiceApi);
+router.use("/", reportApi);
+router.use("/", paymentApi);
+router.use("", supportManageApi);
+router.use("", staffManageApi);
+router.use("", serviceManageApi);
+router.use("", statisticApi);
+router.use("", paymentApi_BuildingOwner);
+router.use("", UserManage);
+router.use("", notificationApi);
+router.use("", apiUser);
+router.use("", adminApi);
+router.use("", postAdmin);
+router.use("", userAdmin);
+router.use("", spAdmin);
+router.use("/staff/posts", post_staffApi); // Đổi đường dẫn cho post_staffApi
+router.use("/staff/contracts", contract_staffApi); // Đổi đường dẫn cho contract_staffApi
+router.use("/staff/payments", payment_staffApi); // Đổi đường dẫn cho contract_staffApi
+router.use("/staff/notifications", notification_staffApi); // Đổi đường dẫn cho contract_staffApi
+router.use("/staff/rooms", room_staffApi); // Đổi đường dẫn cho contract_staffApi
+router.use("/staff/invoices", invoice_staffApi); // Đổi đường dẫn cho contract_staffApi
+router.use("/staff/requests", request_staffApi); // Đổi đường dẫn cho contract_staffApi
+router.use("/", UserManage);
+router.use("/", PostManage);
+router.use("/", PaymentManage);
+router.use("/", ContractManage);
+router.use("/", BuildingManage);
 
 module.exports = router;
