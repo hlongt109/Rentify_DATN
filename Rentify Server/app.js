c726ee9126b12a05884b6e860bba68bc62816596
Nguyen Thien Thien <kinaad24@gmail.com>
2024-11-03 16:02:45 +0700

    thien push usermanager

--- Rentify Server/app.js
+++ Rentify Server/app.js
@@ -20,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to parse URL-encoded bodies
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/api/assets', express.static(path.join(__dirname, 'assets')));
 
 // connect
 app.use('/', indexRouter);
