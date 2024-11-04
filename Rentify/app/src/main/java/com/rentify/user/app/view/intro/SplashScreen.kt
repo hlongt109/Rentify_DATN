c82290f5203014632da12421199935d9cc1ee399
myxcs <not.quangthai.1806@gmail.com>
2024-11-04 22:27:46 +0700

    small fix

--- Rentify/app/src/main/java/com/rentify/user/app/view/intro/SplashScreen.kt
+++ Rentify/app/src/main/java/com/rentify/user/app/view/intro/SplashScreen.kt
@@ -3,12 +3,17 @@ package com.rentify.user.app.view.intro
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rentify.user.app.R
@@ -24,9 +29,15 @@ fun SplashScreen() {
         verticalArrangement = Arrangement.Center
     ) {
         Image(
-            painter = painterResource(id = R.drawable.a),
+            painter = painterResource(id = R.drawable.logo),
             contentDescription = "Intro Screen",
-            modifier = Modifier.size(300.dp)
+            modifier = Modifier.size(150.dp)
+        )
+        Text(
+            text = "Rentify",
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontWeight = FontWeight.Bold
         )
     }
 }
