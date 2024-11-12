655d34339e28cf380277533202446b4468ad85bf
Nguyen Thien Thien <kinaad24@gmail.com>
2024-11-12 17:05:25 +0700

    remote

--- Web_Admin_Rentify/src/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { createRoot } from 'react-dom/client'; // Import createRoot
-import App from './App';
-const rootElement = document.getElementById('root'); // Lấy phần tử DOM
-const root = createRoot(rootElement); // Tạo root
-
-root.render(<App />); // Render ứng dụng
