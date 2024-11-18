d0d410027f82ff14c44cf8db633082489761e37b
Nguyen Thien Thien <kinaad24@gmail.com>
2024-11-19 02:11:52 +0700

    thien push code 19-11

--- Rentify Server/routes/api.js
+++ Rentify Server/routes/api.js
@@ -60,7 +60,7 @@ router.use("/", reportApi);
 router.use("/", paymentApi);
 router.use("", supportManageApi);
 router.use("", staffManageApi);
-router.use("", serviceManageApi);
+router.use("/", serviceManageApi);
 router.use("", statisticApi);
 router.use("", paymentApi_BuildingOwner);
 router.use("", UserManage);
