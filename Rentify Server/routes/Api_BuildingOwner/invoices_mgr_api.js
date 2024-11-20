374ebd7b5ffbb09e61f45458ee39b67a8686fe8d
Tran Hoang Long <hoanglong@smiletech.com>
2024-11-21 01:38:52 +0700

    succes invoice room manager

--- Rentify Server/routes/Api_BuildingOwner/invoices_mgr_api.js
+++ Rentify Server/routes/Api_BuildingOwner/invoices_mgr_api.js
@@ -25,7 +25,7 @@ router.get('/buildings_mgr/:landlord_id', async(req, res) => {
     }
 });
 
-// Route để lấy danh sách hóa đơn cho phòng của một tòa nhà, lọc theo trạng thái thanh toán và tháng/năm
+// Lấy danh sách hóa đơn cho phòng của một tòa nhà, lọc theo trạng thái thanh toán và tháng/năm
 router.get("/buildings_mgr/:buildingId/invoices", async(req, res) => {
     try {
         const { buildingId } = req.params;
@@ -68,5 +68,48 @@ router.get("/buildings_mgr/:buildingId/invoices", async(req, res) => {
         handleServerError(res, error);
     }
 })
+
+// lấy danh sách hóa đơn lọc theo năm
+router.get("/buildings_mgr/:buildingId/invoices/year", async (req, res) => {
+    try {
+        const { buildingId } = req.params;
+        const { year, paymentStatus } = req.query;
+
+        const currentDate = new Date();
+        const selectedYear = year || currentDate.getFullYear(); // Nếu không có năm truyền vào, mặc định lấy năm hiện tại
+
+        if (!buildingId) {
+            return res.status(400).json({ message: 'Building ID trống' });
+        }
+
+        const rooms = await Room.find({ building_id: buildingId }).exec();
+        if (!rooms || rooms.length === 0) {
+            return res.status(404).json({ message: 'Không tìm thấy phòng trong tòa nhà này.' });
+        }
+
+        // Lọc hóa đơn theo năm và trạng thái thanh toán
+        const invoices = await Invoice.find({
+            room_id: { $in: rooms.map(room => room._id) },
+            payment_status: paymentStatus || { $exists: true },
+            created_at: {
+                $gte: `${selectedYear}-01-01T00:00:00`,  // Từ ngày 1 tháng 1 của năm
+                $lt: `${selectedYear + 1}-01-01T00:00:00` // Đến ngày 1 tháng 1 của năm tiếp theo
+            }
+        }).populate('room_id').exec();
+
+        // Tách hóa đơn đã thanh toán và chưa thanh toán
+        const paidInvoices = invoices.filter(invoice => invoice.payment_status === 'paid');
+        const unpaidInvoices = invoices.filter(invoice => invoice.payment_status === 'unpaid');
+
+        return res.status(200).json({
+            paidInvoices,
+            unpaidInvoices
+        });
+
+    } catch (error) {
+        console.error(error);
+        handleServerError(res, error);
+    }
+});
 
 module.exports = router;
