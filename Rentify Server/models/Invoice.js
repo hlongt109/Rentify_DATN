6060b80f486cad8a89e5601f1d7588a313c2bbe1
Tran Hoang Long <hoanglong@smiletech.com>
2024-11-23 02:00:26 +0700

    commit service fee

--- Rentify Server/models/Invoice.js
+++ Rentify Server/models/Invoice.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Invoice = new Schema({
-    user_id: { // id nhân viên thu tiền
+    user_id: { // id nhân viên 
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
+    },
+    building_id: { // cho hóa đơn phí dv, lương, bảo trì
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Building",
     },
     room_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Room',
-        required: true
     },
     description: [{
         service_name: { type: String, required: true }, // tên dịch vụ (ví dụ: điện, nước)
@@ -17,6 +19,7 @@ const Invoice = new Schema({
         price_per_unit: { type: Number, required: true }, // giá mỗi đơn vị
         total: { type: Number, required: true } // tổng chi phí cho dịch vụ đó
     }],
+    type_invoice: {type: String, enum: ["electric", "water", "salary", "maintain"]},
     amount: { type: Number, require: true }, // số tien
     transaction_type: { type: String, enum: ['income', 'expense'], require: true },
     due_date: { type: String }, // hạn chót
--- Rentify Server/routes/Api_BuildingOwner/invoices_mgr_api.js
+++ Rentify Server/routes/Api_BuildingOwner/invoices_mgr_api.js
@@ -6,7 +6,7 @@ const Invoice = require("../../models/Invoice");
 const handleServerError = require("../../utils/errorHandle");
 
 //
-router.get('/buildings_mgr/:landlord_id', async(req, res) => {
+router.get('/buildings_mgr/:landlord_id', async (req, res) => {
     try {
         const { landlord_id } = req.params;
         if (!landlord_id) {
@@ -14,9 +14,9 @@ router.get('/buildings_mgr/:landlord_id', async(req, res) => {
         }
 
         const buildings = await Building.find({ landlord_id: landlord_id })
-        if(buildings){
-            res.status(200).json({data: buildings})
-        }else{
+        if (buildings) {
+            res.status(200).json({ data: buildings })
+        } else {
             return res.status(404).json({ message: "Buildings not found" })
         }
     } catch (error) {
@@ -26,15 +26,15 @@ router.get('/buildings_mgr/:landlord_id', async(req, res) => {
 });
 
 // Lấy danh sách hóa đơn cho phòng của một tòa nhà, lọc theo trạng thái thanh toán và tháng/năm
-router.get("/buildings_mgr/:buildingId/invoices", async(req, res) => {
+router.get("/buildings_mgr/:buildingId/invoices", async (req, res) => {
     try {
         const { buildingId } = req.params;
 
         const { month, year, paymentStatus } = req.query;
-    
+
         const currentDate = new Date();
-        const selectedMonth = month || currentDate.getMonth() + 1; 
-        const selectedYear = year || currentDate.getFullYear(); 
+        const selectedMonth = month || currentDate.getMonth() + 1;
+        const selectedYear = year || currentDate.getFullYear();
 
         if (!buildingId) {
             return res.status(400).json({ message: 'Building ID trống' });
@@ -49,7 +49,7 @@ router.get("/buildings_mgr/:buildingId/invoices", async(req, res) => {
             room_id: { $in: rooms.map(room => room._id) },
             payment_status: paymentStatus || { $exists: true },
             created_at: {
-                $gte: `${selectedYear}-${selectedMonth}-01T00:00:00`, 
+                $gte: `${selectedYear}-${selectedMonth}-01T00:00:00`,
                 $lt: `${selectedYear}-${selectedMonth + 1}-01T00:00:00`
             }
         }).populate('room_id').exec();
@@ -69,7 +69,7 @@ router.get("/buildings_mgr/:buildingId/invoices", async(req, res) => {
     }
 })
 
-// lấy danh sách hóa đơn lọc theo năm
+// lấy danh sách hóa đơn phòng lọc theo năm
 router.get("/buildings_mgr/:buildingId/invoices/year", async (req, res) => {
     try {
         const { buildingId } = req.params;
@@ -111,5 +111,180 @@ router.get("/buildings_mgr/:buildingId/invoices/year", async (req, res) => {
         handleServerError(res, error);
     }
 });
+
+// lọc invoice theo type 
+router.get("/buildings_mgr/:buildingId/invoices_type", async (req, res) => {
+    try {
+        const { buildingId } = req.params;
+
+        const { month, year, paymentStatus, typeInvoice } = req.query;
+
+        const currentDate = new Date();
+        const selectedMonth = month || currentDate.getMonth() + 1;
+        const selectedYear = year || currentDate.getFullYear();
+
+        if (!buildingId) {
+            return res.status(400).json({ message: 'Building ID trống' });
+        }
+        if (!typeInvoice) {
+            return res.status(400).json({ message: 'Type invoice trống' });
+        }
+
+        const validTypes = ['water', 'electric', 'salary', 'maintain'];
+        if (!validTypes.includes(typeInvoice)) {
+            return res.status(400).json({ message: `Loại hóa đơn không hợp lệ. Chỉ chấp nhận: ${validTypes.join(', ')}` });
+        }
+
+
+
+        // Lọc hóa đơn theo tháng/năm và trạng thái thanh toán
+        const invoices = typeInvoice === 'salary'
+            ? await Invoice.find({
+                type_invoice: typeInvoice,
+                payment_status: paymentStatus || { $exists: true },
+                created_at: {
+                    $gte: `${selectedYear}-${selectedMonth}-01T00:00:00`,
+                    $lt: `${selectedYear}-${selectedMonth + 1}-01T00:00:00`
+                }
+            }).populate('user_id').exec()
+            : await Invoice.find({
+                type_invoice: typeInvoice,
+                payment_status: paymentStatus || { $exists: true },
+                created_at: {
+                    $gte: `${selectedYear}-${selectedMonth}-01T00:00:00`,
+                    $lt: `${selectedYear}-${selectedMonth + 1}-01T00:00:00`
+                }
+            }).exec()
+
+        if (typeInvoice === 'water') {
+            invoices.filter(invoice => invoice.type_invoice === 'water');
+        }
+        if (typeInvoice === 'electric') {
+            invoices.filter(invoice => invoice.type_invoice === 'electric');
+        }
+        if (typeInvoice === 'salary') {
+            invoices.filter(invoice => invoice.type_invoice === 'salary');
+        }
+        if (typeInvoice === 'maintain') {
+            invoices.filter(invoice => invoice.type_invoice === 'maintain');
+        }
+
+        // Tách hóa đơn đã thanh toán và chưa thanh toán
+        const paidInvoices = invoices.filter(invoice => invoice.payment_status === 'paid');
+        const unpaidInvoices = invoices.filter(invoice => invoice.payment_status === 'unpaid');
+
+        return res.status(200).json({
+            paidInvoices,
+            unpaidInvoices
+        });
+
+    } catch (error) {
+        console.error(error);
+        handleServerError(res, error);
+    }
+})
+
+// lấy danh sách hóa đơn lọc theo năm
+router.get("/buildings_mgr/:buildingId/invoices_type/year", async (req, res) => {
+    try {
+        const { buildingId } = req.params;
+        const { year, paymentStatus, typeInvoice } = req.query;
+
+        const currentDate = new Date();
+        const selectedYear = year || currentDate.getFullYear();
+
+        if (!buildingId) {
+            return res.status(400).json({ message: 'Building ID trống' });
+        }
+        if (!typeInvoice) {
+            return res.status(400).json({ message: 'Type invoice trống' });
+        }
+
+        const validTypes = ['water', 'electric', 'salary', 'maintain'];
+        if (!validTypes.includes(typeInvoice)) {
+            return res.status(400).json({ message: `Loại hóa đơn không hợp lệ. Chỉ chấp nhận: ${validTypes.join(', ')}` });
+        }
+
+        // Lọc hóa đơn theo tháng/năm và trạng thái thanh toán
+        const invoices = typeInvoice === 'salary'
+            ? await Invoice.find({
+                building_id: buildingId,
+                type_invoice: typeInvoice,
+                payment_status: paymentStatus || { $exists: true },
+                created_at: {
+                    $gte: `${selectedYear}-01-01T00:00:00`,  // Từ ngày 1 tháng 1 của năm
+                    $lt: `${selectedYear + 1}-01-01T00:00:00` // Đến ngày 1 tháng 1 của năm tiếp theo
+                }
+            }).populate('user_id').exec()
+            : await Invoice.find({
+                building_id: buildingId,
+                type_invoice: typeInvoice,
+                payment_status: paymentStatus || { $exists: true },
+                created_at: {
+                    $gte: `${selectedYear}-01-01T00:00:00`,  // Từ ngày 1 tháng 1 của năm
+                    $lt: `${selectedYear + 1}-01-01T00:00:00` // Đến ngày 1 tháng 1 của năm tiếp theo
+                }
+            }).exec()
+
+        if (typeInvoice === 'water') {
+            invoices.filter(invoice => invoice.type_invoice === 'water');
+        }
+        if (typeInvoice === 'electric') {
+            invoices.filter(invoice => invoice.type_invoice === 'electric');
+        }
+        if (typeInvoice === 'salary') {
+            invoices.filter(invoice => invoice.type_invoice === 'salary');
+        }
+        if (typeInvoice === 'maintain') {
+            invoices.filter(invoice => invoice.type_invoice === 'maintain');
+        }
+
+        // Tách hóa đơn đã thanh toán và chưa thanh toán
+        const paidInvoices = invoices.filter(invoice => invoice.payment_status === 'paid');
+        const unpaidInvoices = invoices.filter(invoice => invoice.payment_status === 'unpaid');
+
+        return res.status(200).json({
+            paidInvoices,
+            unpaidInvoices
+        });
+
+    } catch (error) {
+        console.error(error);
+        handleServerError(res, error);
+    }
+});
+
+router.put("/invoice_mgr_status/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const invoice = await Invoice.findById(id)
+        if(!invoice){
+            return res.status(404).json({ message: "Invoice not found" })
+        }
+
+        invoice.user_id = invoice.user_id
+        invoice.building_id =  invoice.building_id
+        invoice.room_id =  invoice.room_id
+        invoice.description = invoice.description 
+        invoice.type_invoice = invoice.type_invoice
+        invoice.amount = invoice.amount
+        invoice.transaction_type = invoice.transaction_type
+        invoice.due_date = invoice.due_date
+        invoice.payment_status = data.payment_status ?? invoice.payment_status
+        invoice.created_at = invoice.created_at
+    
+
+        const result = await invoice.save();
+        if (result) {
+            res.status(200).json({ message: "Update Invoice success", data: result })
+        } else {
+            res.status(401).json({ message: "Update Invoice failed" })
+        }
+    } catch (error) {
+        console.error(error);
+        handleServerError(res, error);
+    }
+})
 
 module.exports = router;
