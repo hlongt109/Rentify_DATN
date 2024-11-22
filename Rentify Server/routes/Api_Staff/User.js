cf577e8ce3c8e09a26d1a08e6ddab651ff1be874
Vũ Văn Phúc <phucvuvan63@gmail.com>
2024-11-22 19:16:29 +0700

    push

---
+++ Rentify Server/routes/Api_Staff/User.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const User = require('../../models/User')
+
+
+
+module.exports = router;
