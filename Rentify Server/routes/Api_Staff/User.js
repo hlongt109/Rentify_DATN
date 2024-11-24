ae0d287b26725f42a54be41aebdb7d87852b8a2c
Vũ Văn Phúc <phucvuvan63@gmail.com>
2024-11-24 23:11:14 +0700

    thông tin cá nhân

--- Rentify Server/routes/Api_Staff/User.js
+++ Rentify Server/routes/Api_Staff/User.js
@@ -4,35 +4,34 @@ const mongoose = require('mongoose');
 
 const User = require('../../models/User')
 
-// LẤY THÔNG TIN NGƯỜI DÙNG 
-router.get('/userr/:id', async (req, res) => {
+  router.get('/usermail/:email', async (req, res) => {
     try {
-      const { id } = req.params;
-  
-      // Tìm người dùng theo ID
-      const user = await User.findById(id);
-  
-      // Kiểm tra nếu người dùng không tồn tại
-      if (!user) {
-        return res.status(404).json({
-          success: false,
-          message: 'Không tìm thấy người dùng'
-        });
-      }
-  
-      // Trả về thông tin chi tiết người dùng
-      res.status(200).json({
-        success: true,
-        data: user
-      });
-    } catch (error) {
-      // Xử lý lỗi
-      console.error(error);
-      res.status(500).json({
-        success: false,
-        message: 'Đã xảy ra lỗi khi lấy thông tin người dùng'
-      });
-    }
-  });
+        // Lấy email từ params
+        const { email } = req.params;
 
+        // Tìm người dùng theo email
+        const user = await User.findOne({ email: email });
+
+        // Kiểm tra nếu người dùng không tồn tại
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'Không tìm thấy người dùng'
+            });
+        }
+
+        // Trả về thông tin chi tiết người dùng
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (error) {
+        // Xử lý lỗi
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: 'Đã xảy ra lỗi khi lấy thông tin người dùng'
+        });
+    }
+});
 module.exports = router;
