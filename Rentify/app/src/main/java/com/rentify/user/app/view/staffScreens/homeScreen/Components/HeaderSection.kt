0868d99b42eddd6aeaa8b8c640054d8c417ec777
Vũ Văn Phúc <phucvuvan63@gmail.com>
2024-12-12 01:06:27 +0700

    thêm ảnh đai dien cho nv

--- Rentify/app/src/main/java/com/rentify/user/app/view/staffScreens/homeScreen/Components/HeaderSection.kt
+++ Rentify/app/src/main/java/com/rentify/user/app/view/staffScreens/homeScreen/Components/HeaderSection.kt
@@ -17,23 +17,50 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import com.rentify.user.app.MainActivity
 import com.rentify.user.app.R
+import com.rentify.user.app.network.RetrofitService
+import com.rentify.user.app.repository.LoginRepository.LoginRepository
+import com.rentify.user.app.viewModel.LoginViewModel
+import com.rentify.user.app.viewModel.UserViewmodel.UserViewModel
 
 @Composable
 fun HeaderSection(navController: NavHostController) {
+    val viewModel: UserViewModel = viewModel()
+    val userDetail by viewModel.user.observeAsState()  // Quan sát LiveData người dùng
+    val errorMessage by viewModel.error.observeAsState()  // Quan sát LiveData lỗi
+    val context = LocalContext.current
+    val apiService = RetrofitService()
+    val userRepository = LoginRepository(apiService)
+    val factory = remember(context) {
+        LoginViewModel.LoginViewModelFactory(userRepository, context.applicationContext)
+    }
+    val loginViewModel: LoginViewModel = viewModel(factory = factory)
+    val userId = loginViewModel.getUserData().userId
+    // Gọi API để lấy thông tin người dùng khi composable được gọi
+    LaunchedEffect(Unit) {
+        viewModel.getUserDetailById(userId)  // Gọi API với userId hợp lệ
+    }
+
     Row(
         modifier = Modifier
             .height(80.dp)
@@ -46,18 +73,45 @@ fun HeaderSection(navController: NavHostController) {
             modifier = Modifier
                 .width(100.dp)
                 .padding(5.dp)
-
-                .clickable(onClick = { /**/ }, indication = null, interactionSource = remember { MutableInteractionSource() }),
+                .clickable(
+                    onClick = { /**/ },
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() }),
             horizontalArrangement = Arrangement.Center
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.staff),
-                contentDescription = null,
-                modifier = Modifier  .clip(CircleShape) // Bo tròn hoàn toàn
-                    .background(Color.White) .size(30.dp, 30.dp)
-                    .clickable { navController.navigate(MainActivity.ROUTER.PersonalStaff.name) }
-            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // Ảnh từ API
+                userDetail?.profile_picture_url?.let { photoUrl ->
+                    val uriAnh = "http://10.0.2.2:3000/$photoUrl"
+                    AsyncImage(
+                        model = uriAnh,
+                        contentDescription = "Profile Picture",
+                        error = painterResource(R.drawable.anhdaidien), // Ảnh lỗi
+                        modifier = Modifier
+                            .size(30.dp)  // Giữ nguyên kích thước ảnh
+                            .clip(CircleShape) // Bo tròn ảnh
+                            .clickable { navController.navigate(MainActivity.ROUTER.PersonalStaff.name) },
+                        contentScale = ContentScale.Crop
+                    )
+                } ?: run {
+                    // Ảnh mặc định nếu không có ảnh từ API
+                    Image(
+                        painter = painterResource(id = R.drawable.staff),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .clip(CircleShape) // Bo tròn hoàn toàn
+                            .background(Color.White)
+                            .size(30.dp, 30.dp) // Đảm bảo kích thước giống như ảnh từ API
+                            .clickable { navController.navigate(MainActivity.ROUTER.PersonalStaff.name) }
+                    )
+                }
+            }
         }
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -79,16 +133,21 @@ fun HeaderSection(navController: NavHostController) {
             modifier = Modifier
                 .width(100.dp)
                 .padding(5.dp)
-                .clickable(onClick = { /**/ }, indication = null, interactionSource = remember { MutableInteractionSource() }),
+                .clickable(
+                    onClick = { /**/ },
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() }),
             horizontalArrangement = Arrangement.Center
         ) {
             Image(
                 painter = painterResource(id = R.drawable.noti),
                 contentDescription = null,
-                modifier = Modifier.size(30.dp, 30.dp)
-                    .clickable {  }
+                modifier = Modifier
+                    .size(30.dp, 30.dp)
+                    .clickable { }
             )
         }
 
     }
 }
+
